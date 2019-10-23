3b9658f458b088c494f17d27e621eef526a52da3
Robert Mugattarov <mugattarov@gmail.com>
2019-10-23 10:24:20 -0700

    updates

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 scripts/jar.sh && \
-$JAVA_HOME/bin/jlink -p app.jar --add-modules image.test --output image
+$JAVA_HOME/bin/jlink -p app.jar --add-modules image.test --launcher run=image.test/rmugattarov.imagetest.App --output image
